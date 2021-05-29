873d2a7ac835cb59fff39db75b772f2be17f0bb4
YuujiKamura <yuujikamura@gmail.com>
2021-05-29 21:15:47 +0900

    triangle area on dart.

--- lib/triangle.dart
+++ lib/triangle.dart
@@ -1,13 +1,20 @@
 
+import 'dart:math' as math;
 
 class Triangle{
   double lengthA_ = 0;
   double lengthB_ = 0;
   double lengthC_ = 0;
+  double sumABC_ = 0;
+  double S_ = 0;
 
   Triangle( double a, double b, double c){
     lengthA_ = a;
     lengthB_ = b;
     lengthC_ = c;
+    sumABC_ = lengthA_ + lengthB_ + lengthC_;
+    S_ = (sumABC_*0.5)*((sumABC_*0.5)- lengthA_)*((sumABC_*0.5)- lengthB_)*((sumABC_*0.5)- lengthC_);
   }
+
+  num get area => math.pow( S_, 0.5);
 }
--- test/triangle_test.dart
+++ test/triangle_test.dart
@@ -2,6 +2,11 @@ import 'package:test/test.dart';
 import 'package:trianglelist/triangle.dart';
 
 void main() {
+  test('get area test', (){
+    Triangle tri = Triangle( 3, 4, 5);
+    expect(tri.area, 6);
+  });
+
   test('triangle test', (){
     Triangle tri = Triangle( 5, 5, 5);
     expect(tri.lengthA_, 5);
